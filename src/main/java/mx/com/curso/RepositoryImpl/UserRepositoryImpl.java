e85d0eefb7d4426cf95b67a5641400f6567ad8a0
Alexis4152 <alekszyzz@hotmail.com>
2024-07-19 22:45:39 -0600

    2024/07/19|LUNA|Correcion a consulta update y getById

--- src/main/java/mx/com/curso/RepositoryImpl/UserRepositoryImpl.java
+++ src/main/java/mx/com/curso/RepositoryImpl/UserRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,45 +16,63 @@ import mx.com.curso.mapper.EmpleadosMapper;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
-	
 	// JDBC TEMPLATE - NECESITA UNA CONEXIÓN A BASE DE DATOS
 
 	@Autowired
 	private DataSource dataSource;
-	
+
 	private JdbcTemplate jdbcTemplate = new JdbcTemplate();
-	
+
 	@Override
 	public List<EmpleadoDto> getUsers() {
-        jdbcTemplate.setDataSource(getDataSource());
-		return jdbcTemplate.query("SELECT * FROM USUARIOS", new EmpleadosMapper<EmpleadoDto>() );
+		jdbcTemplate.setDataSource(getDataSource());
+		return jdbcTemplate.query("SELECT * FROM USUARIOS", new EmpleadosMapper<EmpleadoDto>());
 	}
-	
+
 	@Override
 	public Integer insertEmpleado(EmpleadoDto empleado) {
-        jdbcTemplate.setDataSource(getDataSource());
+		jdbcTemplate.setDataSource(getDataSource());
 		return jdbcTemplate.update("INSERT INTO USUARIOS(ID_ALUMNO, NOMBRE, SALARIO, EDAD) VALUES (?,?,?,?)",
-        		new Object[] {empleado.getIdAlumno(), empleado.getNombre(), empleado.getSalario(), empleado.getEdad()} );
+				new Object[] { empleado.getIdAlumno(), empleado.getNombre(), empleado.getSalario(),
+						empleado.getEdad() });
 	}
 
 	@Override
 	public Integer updateEmpleado(EmpleadoDto empleado) {
-	    jdbcTemplate.setDataSource(getDataSource());
-		return jdbcTemplate.update("UPDATE USUARIOS SET NOMBRE =?, SALARIO = ?, EDAD = ? WHERE ID_ALUMNO = ?"
-				, new Object[] {empleado.getNombre(), empleado.getSalario(), empleado.getEdad()} );
+		jdbcTemplate.setDataSource(getDataSource());
+		return jdbcTemplate.update("UPDATE USUARIOS SET NOMBRE =?, SALARIO = ?, EDAD = ? WHERE ID_ALUMNO = ?",
+				new Object[] { empleado.getNombre(), empleado.getSalario(), empleado.getEdad(),
+						empleado.getIdAlumno() });
 	}
-	
+
 	@Override
 	public Integer deleteEmpleado(EmpleadoDto empleado) {
 		jdbcTemplate.setDataSource(getDataSource());
-		return jdbcTemplate.update("DELETE FROM USUARIOS WHERE ID_ALUMNO = ? ", new Object[] {empleado.getIdAlumno()} );
+		return jdbcTemplate.update("DELETE FROM USUARIOS WHERE ID_ALUMNO = ? ",
+				new Object[] { empleado.getIdAlumno() });
 	}
-
 
 	@Override
 	public EmpleadoDto getEmpleadoById(Integer idAlumno) {
-		jdbcTemplate.setDataSource(getDataSource());
-		return jdbcTemplate.queryForObject("SELECT * FROM USUARIOS WHERE ID_ALUMNO = ? ", new Object[] {idAlumno}, new EmpleadosMapper<EmpleadoDto>());
+		EmpleadoDto result = new EmpleadoDto();
+
+		try {
+			jdbcTemplate.setDataSource(getDataSource());
+
+			result = jdbcTemplate.queryForObject("SELECT * FROM USUARIOS WHERE ID_ALUMNO = ? ",
+					new Object[] { idAlumno }, new EmpleadosMapper<EmpleadoDto>());
+
+		} catch (EmptyResultDataAccessException e) {
+			result = null;
+			System.out.println("Sucedio una excepcion: " + e.getMessage() + " no se obtuvieron resultados");
+		} catch (NullPointerException npe) {
+			result = null;
+			System.out.println("Sucedio una excepcion: " + npe.getMessage() + " algun valor se encuentra como nulo");
+		} catch (Exception e) {
+			System.out.println("Sucedio una excepcion: " + e.getMessage());
+		}
+
+		return result;
 	}
 
 	public DataSource getDataSource() {
@@ -71,15 +90,5 @@ public class UserRepositoryImpl implements UserRepository {
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-
-
-	
-	
-
-
-
-	
-	
-	
 
 }
--- src/main/java/mx/com/curso/ServiceImpl/UserServiceImpl.java
+++ src/main/java/mx/com/curso/ServiceImpl/UserServiceImpl.java
@@ -9,14 +9,16 @@ import mx.com.curso.Dto.EmpleadoDto;
 import mx.com.curso.Dto.ResponseDto;
 import mx.com.curso.Repository.UserRepository;
 import mx.com.curso.Service.UserService;
+import mx.com.curso.util.Util;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-	
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	Util objeo = new Util();
+
 	@Override
 	public List<EmpleadoDto> getUsers() {
 		// TODO Auto-generated method stub
@@ -26,17 +28,17 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public ResponseDto insertEmpleado(EmpleadoDto empleado) {
 		ResponseDto response = new ResponseDto();
-	     Integer respuestaInsercion = userRepository.insertEmpleado(empleado);
-	     
-	     if(respuestaInsercion == 1) {
-	    	 response.setCode(1);
-	    	 response.setMessage("Se insertó correctamente el empleado");
-	     }
-	     else {
-	    	 response.setCode(0);
-	    	 response.setMessage("No se insertó el empleado, favor de validar");
-	     }
-	    
+
+		Integer respuestaInsercion = userRepository.insertEmpleado(empleado);
+
+		if (respuestaInsercion == 1) {
+			response.setCode(1);
+			response.setMessage("Se insertó correctamente el empleado");
+		} else {
+			response.setCode(0);
+			response.setMessage("No se insertó el empleado, favor de validar");
+		}
+
 		return response;
 	}
 
@@ -44,15 +46,14 @@ public class UserServiceImpl implements UserService {
 	public ResponseDto updateEmpleado(EmpleadoDto empleado) {
 		ResponseDto response = new ResponseDto();
 		Integer respuestInsert = userRepository.updateEmpleado(empleado);
-		
-		 if(respuestInsert == 1) {
-	    	 response.setCode(1);//esto es un comentario
-	    	 response.setMessage("Se actualizo el empleado");
-	     }
-		 else {
-	    	 response.setCode(0);
-	    	 response.setMessage("No se actualizo el empleado, favor de validar");
-	     }
+
+		if (respuestInsert == 1) {
+			response.setCode(1);// esto es un comentario
+			response.setMessage("Se actualizo el empleado");
+		} else {
+			response.setCode(0);
+			response.setMessage("No se actualizo el empleado, favor de validar");
+		}
 		return response;
 	}
 
@@ -60,34 +61,32 @@ public class UserServiceImpl implements UserService {
 	public ResponseDto deleteEmpleado(EmpleadoDto empleado) {
 		ResponseDto response = new ResponseDto();
 		Integer respuestaDelete = userRepository.deleteEmpleado(empleado);
-		
-		 if(respuestaDelete == 1) {
-	    	 response.setCode(1);//esto es un comentario
-	    	 response.setMessage("Se eliminó al empleado");
-	     }
-		 else {
-	    	 response.setCode(0);
-	    	 response.setMessage("No se pudo eliminar al empleado, favor de validar");
-	     }
+
+		if (respuestaDelete == 1) {
+			response.setCode(1);// esto es un comentario
+			response.setMessage("Se eliminó al empleado");
+		} else {
+			response.setCode(0);
+			response.setMessage("No se pudo eliminar al empleado, favor de validar");
+		}
 		return response;
 	}
 
 	@Override
 	public ResponseDto getEmpleadoById(Integer idAlumno) {
 		ResponseDto response = new ResponseDto();
-		
+
 		EmpleadoDto empleadoRespuesta = userRepository.getEmpleadoById(idAlumno);
-		
-		if(empleadoRespuesta != null ) {
+
+		if (empleadoRespuesta != null) {
 			response.setCode(1);
 			response.setContent(empleadoRespuesta);
 			response.setMessage("Empleado obtenido");
-		}
-		else {
+		} else {
 			response.setCode(0);
 			response.setMessage("No se pudo obtener al empleado");
 		}
-		
+
 		return response;
 	}
 
