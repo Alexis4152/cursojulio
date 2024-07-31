543979f1ff404793692da5de89c84c8b6d68d3ca
JonathanRDZ230 <jonathanro39@gmail.com>
2024-07-30 20:06:49 -0600

    Actualizacion del servicio de TransaccionesImpl y simplificacion de las validaciones|30/07/2024|Jonathan

--- src/main/java/mx/com/curso/ServiceImpl/TransaccionesServiceImpl.java
+++ src/main/java/mx/com/curso/ServiceImpl/TransaccionesServiceImpl.java
@@ -1,9 +1,5 @@
 package mx.com.curso.ServiceImpl;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +10,7 @@ import mx.com.curso.Dto.TransaccionesAprobadasDto;
 import mx.com.curso.Dto.TransaccionesDto;
 import mx.com.curso.Repository.TransaccionesRepository;
 import mx.com.curso.Service.TransaccionesService;
+import mx.com.curso.util.ValidacionesTransaccionesUtil;
 
 @Service
 public class TransaccionesServiceImpl implements TransaccionesService {
@@ -30,99 +27,18 @@ public class TransaccionesServiceImpl implements TransaccionesService {
 	@Override
 	public ResponseDto insertTransaccion(TransaccionesDto transaccion) {
 		//SIMPLIFICAR TODO ESTO, METERLO EN LA PAQUETERIA UTIL Y TRAERLO AQUI EN EL SERVICE
-		ResponseDto response = new ResponseDto();
-		BigInteger maxValue = new BigInteger("99999999999999999999");
-		BigDecimal maxCantidad = new BigDecimal("99999999.99");
-		
-		LocalDate fechaActual = LocalDate.now();
-		LocalDate fecha = transaccion.getFecha().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-		
-		if (transaccion.getIdTransaccion() == null) {
-			response.setCode(0);
-			response.setMessage("Error!: No puede ser Nulo la ID_TRANSACCION");
-			return response;
-		} else if (transaccion.getIdTransaccion().compareTo(maxValue) > 0) {
-			response.setCode(0);
-			response.setMessage(
-					"Error!, te has excedido con rango de la ID_TRANSACCION, solo acepta un maximo de 20 digitos");
-			return response;
-		}
-						
-		if(fecha.isAfter(fechaActual)) {
-			response.setCode(0);
-			response.setMessage("Error!: No puedes modificar la fecha para futuros");
-			return response;
-		}
-		if (transaccion.getCantidad() == null) {
-			response.setCode(0);
-			response.setMessage("Error!: No puede ser Nulo el MONTO");
-			return response;
-		} else if (transaccion.getCantidad().compareTo(maxCantidad) > 0) {
-			response.setCode(0);
-			response.setMessage("Error!, te has excedido con rango del MONTO, solo acepta un maximo de 10 digitos");
-			return response;
-		}
-		
-		if (transaccion.getMoneda() == null) {
-			response.setCode(0);
-			response.setMessage("Error!: No puede ser Nulo la MONEDA");
-			return response;
-		} else if (transaccion.getMoneda().length() > 3 || transaccion.getMoneda().length() < 3) {
-			response.setCode(0);
-			response.setMessage("Error!: Debe agregar solo 3 letras, no mas, no menos, ejemplo EUR, USD, MXN, etc...");
-			return response;
-		}		
-		
-		if (transaccion.getMetodoPago() == null) {
-			response.setCode(0);
-			response.setMessage("Error!: No puede ser Nulo el METODO DE PAGO");
-			return response;
-		} else if (transaccion.getMetodoPago().length() > 20) {
-			response.setCode(0);
-			response.setMessage("Error!: Debe respetar el rango con el METODO DE PAGO, solo acepta hasta 20 caracteres");
-			return response;
-		}
-			
-		if (transaccion.getDetallePago() == null) {
-			response.setCode(0);
-			response.setMessage("Error!: No puede ser Nulo el DETALLE DE PAGO");
-			return response;
-		} else if (transaccion.getDetallePago().length() > 25) {
-			response.setCode(0);
-			response.setMessage(
-					"Error!: Excedio el rango con el DETALLE DE PAGO, solo se admiten 25 caracteres en este apartado, intenta de nuevo");
-			return response;
-		}
-		if (transaccion.getConcepto() == null) {
-			response.setCode(0);
-			response.setMessage("Error!: No puede ser Nulo el CONCEPTO de la transferencia");
-			return response;
-		} else if (transaccion.getConcepto().length() > 20) {
-			response.setCode(0);
-			response.setMessage(
-					"Error!: Excedio el rango con el CONCEPTO, solo se admiten 20 caracteres en este apartado, intenta de nuevo");
-			return response;
-		}
-		if (transaccion.getNombreSucursalEmisor() == null) {
-			response.setCode(0);
-			response.setMessage("Error!: No puede ser Nulo el NOMBRE DE LA SUCURSAL EMISORA de la transferencia");
-			return response;
-		} else if (transaccion.getNombreSucursalEmisor().length() > 15) {
-			response.setCode(0);
-			response.setMessage(
-					"Error!: Excedio el rango con el NOMBRE DE LA SUCURSAL EMISORA, solo se admiten 15 caracteres en este apartado, intenta de nuevo");
-			return response;
-		}
-		if (transaccion.getNombreSucursalReceptor() == null) {
-			response.setCode(0);
-			response.setMessage("Error!: No puede ser Nulo el NOMBRE DE LA SUCURSAL RECEPTORA de la transferencia");
-			return response;
-		} else if (transaccion.getNombreSucursalReceptor().length() > 15) {
-			response.setCode(0);
-			response.setMessage(
-					"Error!: Excedio el rango con el NOMBRE DE LA SUCURSAL RECEPTORA, solo se admiten 15 caracteres en este apartado, intenta de nuevo");
-			return response;
-		}
+		ResponseDto response = new ResponseDto();	
+		ValidacionesTransaccionesUtil validacion = new ValidacionesTransaccionesUtil(transaccion);
+					
+		response = validacion.validacionIdTransaccion();
+		response = validacion.validacionFecha();
+		response = validacion.validacionCantidad();
+		response = validacion.validacionMoneda();
+		response = validacion.validacionMetodoPago();
+		response = validacion.validacionDetallePago();
+		response = validacion.validacionNombreSucursal();
+		if (response.getCode() == 0) return response;
+							
 		if (transaccion.getIdCuentaTransaccion() == null) {
 			response.setCode(0);
 			response.setMessage("Error!: No puede ser Nulo la ID_USUARIO");
@@ -131,10 +47,10 @@ public class TransaccionesServiceImpl implements TransaccionesService {
 			Integer respuestaInsercion = transaccionesRepository.insertTransaccion(transaccion);
 			if (respuestaInsercion == 1) {
 				response.setCode(1);
-				response.setMessage("Se insertó correctamente la transacción");
+				response.setMessage("Se inserto correctamente la transaccion");
 			} else {
 				response.setCode(0);
-				response.setMessage("No se insertó la transacción, favor de validar");
+				response.setMessage("No se inserto la transaccion, favor de validar");
 			}
 
 		}
@@ -149,10 +65,10 @@ public class TransaccionesServiceImpl implements TransaccionesService {
 
 		if (respuestaActualizacion == 1) {
 			response.setCode(1);
-			response.setMessage("Se actualizo correctamente la transacción");
+			response.setMessage("Se actualizo correctamente la transacciï¿½n");
 		} else {
 			response.setCode(0);
-			response.setMessage("No se actualizo correctamente la transacción, favor de validar");
+			response.setMessage("No se actualizo correctamente la transacciï¿½n, favor de validar");
 		}
 
 		return response;
@@ -166,10 +82,10 @@ public class TransaccionesServiceImpl implements TransaccionesService {
 
 		if (respuestaEliminacion == 1) {
 			response.setCode(1);
-			response.setMessage("Se elimino correctamente la transacción");
+			response.setMessage("Se elimino correctamente la transacciï¿½n");
 		} else {
 			response.setCode(0);
-			response.setMessage("Error!, no se pudo eliminar la transacción, favor de validar");
+			response.setMessage("Error!, no se pudo eliminar la transacciï¿½n, favor de validar");
 		}
 		return response;
 	}
--- src/main/java/mx/com/curso/util/ValidacionesTransaccionesUtil.java
+++ src/main/java/mx/com/curso/util/ValidacionesTransaccionesUtil.java
@@ -1,15 +1,24 @@
 package mx.com.curso.util;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import mx.com.curso.Dto.ResponseDto;
 import mx.com.curso.Dto.TransaccionesDto;
 
 public class ValidacionesTransaccionesUtil {
-	
-	public ResponseDto validacionIdTransaccion(TransaccionesDto transaccion) {
-		ResponseDto response = new ResponseDto();
-		BigInteger maxValue = new BigInteger("99999999999999999999");
+	private TransaccionesDto transaccion;
+	ResponseDto response = new ResponseDto();
+	BigInteger maxValue = new BigInteger("99999999999999999999");
+	BigDecimal maxCantidad = new BigDecimal("99999999.99");
+
+	public ValidacionesTransaccionesUtil(TransaccionesDto transaccion) {
+		this.transaccion = transaccion;
+	}
+
+	public ResponseDto validacionIdTransaccion() {
 		if (transaccion.getIdTransaccion() == null) {
 			response.setCode(0);
 			response.setMessage("Error!: No puede ser Nulo la ID_TRANSACCION");
@@ -22,4 +31,107 @@ public class ValidacionesTransaccionesUtil {
 		}
 		return response;
 	}
+	
+	public ResponseDto validacionFecha() {
+		LocalDate fechaActual = LocalDate.now();
+		LocalDate fecha = transaccion.getFecha().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		if(fecha.isAfter(fechaActual)) {
+			response.setCode(0);
+			response.setMessage("Error!: No puedes modificar la fecha para futuros");
+			return response; 
+		}
+		return response;
+	}
+
+	public ResponseDto validacionCantidad() {
+		if (transaccion.getCantidad() == null) {
+			response.setCode(0);
+			response.setMessage("Error!: No puede ser Nulo el MONTO");
+			return response;
+		} else if (transaccion.getCantidad().compareTo(maxCantidad) > 0) {
+			response.setCode(0);
+			response.setMessage("Error!, te has excedido con rango del MONTO, solo acepta un maximo de 10 digitos");
+			return response;
+		}
+		return response;
+	}
+
+	public ResponseDto validacionMoneda() {
+		if (transaccion.getMoneda() == null) {
+			response.setCode(0);
+			response.setMessage("Error!: No puede ser Nulo la MONEDA");
+			return response;
+		} else if (transaccion.getMoneda().length() > 3 || transaccion.getMoneda().length() < 3) {
+			response.setCode(0);
+			response.setMessage("Error!: Debe agregar solo 3 letras, no mas, no menos, ejemplo EUR, USD, MXN, etc...");
+			return response;
+		}
+		return response;
+	}
+
+	public ResponseDto validacionMetodoPago() {
+		if (transaccion.getMetodoPago() == null) {
+			response.setCode(0);
+			response.setMessage("Error!: No puede ser Nulo el METODO DE PAGO");
+			return response;
+		} else if (transaccion.getMetodoPago().length() > 20) {
+			response.setCode(0);
+			response.setMessage(
+					"Error!: Debe respetar el rango con el METODO DE PAGO, solo acepta hasta 20 caracteres");
+			return response;
+		}
+		return response;
+	}
+
+	public ResponseDto validacionDetallePago() {
+		if (transaccion.getDetallePago() == null) {
+			response.setCode(0);
+			response.setMessage("Error!: No puede ser Nulo el DETALLE DE PAGO");
+			return response;
+		} else if (transaccion.getDetallePago().length() > 25) {
+			response.setCode(0);
+			response.setMessage(
+					"Error!: Excedio el rango con el DETALLE DE PAGO, solo se admiten 25 caracteres en este apartado, intenta de nuevo");
+			return response;
+		}
+		return response;
+	}
+
+	public ResponseDto validacionConcepto() {
+		if (transaccion.getConcepto() == null) {
+			response.setCode(0);
+			response.setMessage("Error!: No puede ser Nulo el CONCEPTO de la transferencia");
+			return response;
+		} else if (transaccion.getConcepto().length() > 20) {
+			response.setCode(0);
+			response.setMessage(
+					"Error!: Excedio el rango con el CONCEPTO, solo se admiten 20 caracteres en este apartado, intenta de nuevo");
+			return response;
+		}
+		return response;
+	}
+
+	public ResponseDto validacionNombreSucursal() {
+		if (transaccion.getNombreSucursalEmisor() == null) {
+			response.setCode(0);
+			response.setMessage("Error!: No puede ser Nulo el NOMBRE DE LA SUCURSAL EMISORA de la transferencia");
+			return response;
+		} else if (transaccion.getNombreSucursalReceptor() == null) {
+			response.setCode(0);
+			response.setMessage("Error!: No puede ser Nulo el NOMBRE DE LA SUCURSAL RECEPTORA de la transferencia");
+			return response;
+		} else if (transaccion.getNombreSucursalEmisor().length() > 15) {
+			response.setCode(0);
+			response.setMessage(
+					"Error!: Excedio el rango con el NOMBRE DE LA SUCURSAL EMISORA, solo se admiten 15 caracteres en este apartado, intenta de nuevo");
+			return response;
+		} else if (transaccion.getNombreSucursalReceptor().length() > 15) {
+			response.setCode(0);
+			response.setMessage(
+					"Error!: Excedio el rango con el NOMBRE DE LA SUCURSAL RECEPTORA, solo se admiten 15 caracteres en este apartado, intenta de nuevo");
+			return response;
+		}
+		return response;
+	}
+
 }
